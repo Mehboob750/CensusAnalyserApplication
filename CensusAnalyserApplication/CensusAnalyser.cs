39e09385b48d9744d11fe8839d95ad22980533e8
Mehboob750 <ms568203@gmail.com>
2020-06-12 12:01:23 +0530

    Refactored: Handled DRY Violation at sorting Function

--- CensusAnalyserApplication/CensusAnalyser.cs
+++ CensusAnalyserApplication/CensusAnalyser.cs
@@ -55,49 +55,82 @@ namespace CensusAnalyserApplication
             return censusList.Count();
         }
 
+        /// <summary>
+        /// This Method is used to check whether the list is empty if empty it will through Exception
+        /// </summary>
+        /// <param name="censusList">Parameter CensusList contains the Loaded Data</param>
+        /// <returns>if it has data it returns true</returns>
+        public bool CheckListEmpty(List<CensusDAO> censusList)
+        {
+            if (censusList == null || censusList.Count() == 0)
+                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.UnableToParse, "No Census Data");
+            return true;
+        }
+
+        /// <summary>
+        /// This Method is used to Convert the Sorted List Data into Json Data Format
+        /// </summary>
+        /// <param name="list">Parameter List Contains the sorted result </param>
+        /// <returns>It returns the Data in Json Format</returns>
+        public string ListDataToJsonData(object list)
+        {
+            string dataInJsonFormat = JsonConvert.SerializeObject(list);
+            return dataInJsonFormat;
+        }
+
+        /// <summary>
+        /// This Method Used To Sort Census Data by State Name
+        /// </summary>
+        /// <returns>It returns the Sorted List to ListDataToJsonData Method</returns>
         public string GetStateWiseSortedCensusData()
         {
-            if (censusList == null || censusList.Count() == 0)
-                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.UnableToParse, "No Census Data");
+            CheckListEmpty(censusList);
             object listAlphabetically = censusList.OrderBy(x => x.state);
-            string dataInJsonFormat = JsonConvert.SerializeObject(listAlphabetically);
-            return dataInJsonFormat;
+            return ListDataToJsonData(listAlphabetically);
         }
 
+        /// <summary>
+        /// This Method Used To Sort State Code  Data by State Code
+        /// </summary>
+        /// <returns>It returns the Sorted List to ListDataToJsonData Method</returns>
         public string GetStateCodeWiseSortedCensusData()
         {
-            if (censusList == null || censusList.Count() == 0)
-                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.UnableToParse, "No Census Data");
-            object listAlphabetically = censusList.OrderBy(x => x.stateCode);
-            string dataInJsonFormat = JsonConvert.SerializeObject(listAlphabetically);
-            return dataInJsonFormat;
+            CheckListEmpty(censusList);
+            object listByState = censusList.OrderBy(x => x.state);
+            return ListDataToJsonData(listByState);
         }
 
+        /// <summary>
+        /// This Method Used To Sort Census Data by State Population
+        /// </summary>
+        /// <returns>It returns the Sorted List to ListDataToJsonData Method</returns>
         public string GetPopulationWiseSortedCensusData()
         {
-            if (censusList == null || censusList.Count() == 0)
-                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.UnableToParse, "No Census Data");
+            CheckListEmpty(censusList);
             object listByPopulation = censusList.OrderBy(x => x.population).Reverse();
-            string dataInJsonFormat = JsonConvert.SerializeObject(listByPopulation);
-            return dataInJsonFormat;
+            return ListDataToJsonData(listByPopulation);
         }
 
+        /// <summary>
+        /// This Method Used To Sort Census Data by Density
+        /// </summary>
+        /// <returns>It returns the Sorted List to ListDataToJsonData Method</returns>
         public string GetPopulationDensityWiseSortedCensusData()
         {
-            if (censusList == null || censusList.Count() == 0)
-                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.UnableToParse, "No Census Data");
+            CheckListEmpty(censusList);
             object listByPopulationDensity = censusList.OrderBy(x => x.densityPerSqKm).Reverse();
-            string dataInJsonFormat = JsonConvert.SerializeObject(listByPopulationDensity);
-            return dataInJsonFormat;
+            return ListDataToJsonData(listByPopulationDensity);
         }
 
+        /// <summary>
+        /// This Method Used To Sort Census Data by State Area
+        /// </summary>
+        /// <returns>It returns the Sorted List to ListDataToJsonData Method</returns>
         public string GetAreaWiseSortedCensusData()
         {
-            if (censusList == null || censusList.Count() == 0)
-                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.UnableToParse, "No Census Data");
+            CheckListEmpty(censusList);
             object listByArea = censusList.OrderBy(x => x.areaInSqKm).Reverse();
-            string dataInJsonFormat= JsonConvert.SerializeObject(listByArea);
-            return dataInJsonFormat;
+            return ListDataToJsonData(listByArea);
         }
     }
 }
